43ab1e8f33232416a42537a326e8568230f2e010
hofmeijerq <q.hofmeijer@outlook.com>
2023-01-18 09:35:56 +0100

    reset web.php

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-//Route::middleware([
-//    'auth:sanctum',
-//    config('jetstream.auth_session'),
-//    'verified',
-//])->group(function () {
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
     Route::get('/', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
-//});
+});
