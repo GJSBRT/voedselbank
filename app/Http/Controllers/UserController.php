644aea1f856cc509cc1ee6a8f33fa677a119b604
Nikai117 <97072138@st.deltion.nl>
2023-01-23 13:15:49 +0100

    Refactored the other %50

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,37 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Classes\Role;
+use App\Http\Requests\CreateUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class UserController extends Controller
 {
-    public function index(Request $request) 
+    public function index(Request $request)
     {
         $permission = Role::checkPermission($request->user(), 'users:read');
         if ($permission) { return $permission; }
 
-        $users = User::paginate();   
-        
+        $users = User::paginate();
+
         return Inertia::render('Users/Show', [
             'users' => $users,
-        ]); 
+        ]);
     }
-    
-    public function new(Request $request) 
+
+    public function new(Request $request)
     {
         $permission = Role::checkPermission($request->user(), 'users:write');
         if ($permission) { return $permission; }
-        
+
         return Inertia::render('Users/New');
     }
-    
-    public function view(Request $request, int $userId) 
+
+    public function view(Request $request, int $userId)
     {
         $permission = Role::checkPermission($request->user(), 'users:read');
         if ($permission) { return $permission; }
-        
+
         $user = User::with('role')->find($userId);
 
         return Inertia::render('Users/View', [
@@ -42,24 +43,16 @@ class UserController extends Controller
         ]);
     }
 
-    public function create(Request $request) 
+    public function create(CreateUserRequest $request)
     {
         $permission = Role::checkPermission($request->user(), 'users:create');
         if ($permission) { return $permission; }
-        
+
         $firstName = $request->input('first_name');
         $lastName = $request->input('last_name');
         $email = $request->input('email');
         $password = $request->input('password');
         $role_id = $request->input('role_id');
-
-        $request->validate([
-            'first_name' => 'required|string|max:100',
-            'last_name' => 'required|string|max:100',
-            'email' => 'required|email|max:100',
-            'password' => 'required|string|max:100',
-            'role_id' => 'required|int',
-        ]);
 
         User::create([
             'first_name' => $firstName,
@@ -72,11 +65,11 @@ class UserController extends Controller
         return redirect()->route('users.index')->banner("{$firstName} is successvol toegevoeged als medewerker!");
     }
 
-    public function update(Request $request, int $userId) 
+    public function update(Request $request, int $userId)
     {
         $permission = Role::checkPermission($request->user(), 'users:update');
         if ($permission) { return $permission; }
-        
+
         $user = User::find($userId);
         $newUser = $request->input('user') ?? null;
         $twoFactorEnabled = $request->input('two_factor_enabled') ?? null;
@@ -100,12 +93,12 @@ class UserController extends Controller
 
         return redirect()->route('users.index')->banner('Mederwerker is successvol aangepast!');
     }
-    
-    public function delete(Request $request, int $userId) 
+
+    public function delete(Request $request, int $userId)
     {
         $permission = Role::checkPermission($request->user(), 'users:delete');
         if ($permission) { return $permission; }
-        
+
         $user = User::find($userId);
         $user->delete();
 
--- resources/js/Pages/Users/New.vue
+++ resources/js/Pages/Users/New.vue
@@ -6,6 +6,7 @@ import { useForm } from '@inertiajs/inertia-vue3';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import RoleSearch from '@/Components/Search/RoleSearch.vue';
+import InputError from '@/Components/InputError.vue';
 
 const form = useForm({
     _method: 'POST',
@@ -59,11 +60,13 @@ const setRole = (role) => {
                                 <div>
                                     <InputLabel for="first_name" value="Voornaam" />
                                     <TextInput id="first_name" v-model="form.first_name" type="text" required autofocus class="mt-1 block w-full"/>
+                                    <InputError :message="form.errors.first_name" class="mt-2"/>
                                 </div>
 
                                 <div>
                                     <InputLabel for="last_name" value="Achternaam" />
                                     <TextInput id="last_name" v-model="form.last_name" type="text" required class="mt-1 block w-full"/>
+                                    <InputError :message="form.errors.last_name" class="mt-2"/>
                                 </div>
                             </div>
 
@@ -71,20 +74,23 @@ const setRole = (role) => {
                                 <div>
                                     <InputLabel for="email" value="Email" />
                                     <TextInput id="email" v-model="form.email" type="email" required class="mt-1 block w-full"/>
+                                    <InputError :message="form.errors.email" class="mt-2"/>
                                 </div>
 
                                 <div>
                                     <InputLabel for="role" value="Rol" class="mb-1"/>
                                     <RoleSearch id="role" :callback="setRole"/>
+                                    <InputError :message="form.errors.role" class="mt-2"/>
                                 </div>
                             </div>
 
                             <div class="col-span-6">
                                 <InputLabel for="password" value="Wachtwoord" />
                                 <TextInput id="password" v-model="form.password" type="password" required class="mt-1 block w-full"/>
+                                <InputError :message="form.errors.password" class="mt-2"/>
                             </div>
                         </template>
-                        
+
                         <template #actions>
                             <PrimaryButton @click="handleSubmit">
                                 Medewerker Toevoegen
