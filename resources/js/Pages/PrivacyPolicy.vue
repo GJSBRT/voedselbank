1b0dd80d488762a71f421f1e20343743626bb55b
Gijsbert <gijsbert@dutchis.net>
2023-01-23 13:31:24 +0100

    Update PrivacyPolicy.vue

--- resources/js/Pages/PrivacyPolicy.vue
+++ resources/js/Pages/PrivacyPolicy.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { Head } from '@inertiajs/inertia-vue3';
 import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
-import PrimaryButton from '../Components/PrimaryButton.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
 import { Inertia } from '@inertiajs/inertia';
 
 defineProps({
