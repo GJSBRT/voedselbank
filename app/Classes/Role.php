faeb410dc61b0270bd321ee612552800243f204a
Nikai117 <97072138@st.deltion.nl>
2023-01-23 14:11:56 +0100

    Miss conflict

--- app/Classes/Role.php
+++ app/Classes/Role.php
@@ -5,23 +5,67 @@ namespace App\Classes;
 use App\Models\User;
 
 class Role {
-    
+
+    /**
+     * All permissions available
+     *
+     * @var array
+     */
+    public static $permissions = [
+        '*', // All permissions
+        'food-packages:read',
+        'food-packages:create',
+        'food-packages:delete',
+        'food-packages:update',
+        'users:read',
+        'users:create',
+        'users:delete',
+        'users:update',
+        'roles:read',
+        'roles:create',
+        'roles:delete',
+        'roles:update',
+        'suppliers:read',
+        'suppliers:create',
+        'suppliers:delete',
+        'suppliers:update',
+        'deliveries:read',
+        'deliveries:create',
+        'deliveries:delete',
+        'deliveries:update',
+        'customers:read',
+        'customers:create',
+        'customers:delete',
+        'customers:update',
+        'categories:read',
+        'categories:create',
+        'categories:update',
+        'categories:delete',
+        ];
+
     /**
      * Check if user has permission
-     * 
+     *
      * @param User $user
      * @param string $permission
-     * @return bool
+     * @param bool $redirect
+     * @return redirect
      */
-    public static function checkPermission(User $user, string $permission): bool {
-        $userRoles = $user->roles;
+    public static function checkPermission(User $user, string $permission, $redirect = true) {
+        $userPermissions = json_decode($user->role->permissions);
 
-        foreach($userRoles as $role) {
-            if (in_array($permission, $role->permissions)) {
-                return true;
+        if (!in_array('*', $userPermissions)) {
+            if (!in_array($permission, $userPermissions)) {
+                if ($redirect) {
+                    return redirect()->route('dashboard')->dangerBanner('Je hebt geen toegang tot deze pagina!');
+                } else {
+                    return false;
+                }
             }
         }
 
-        return false;
+        if (!$redirect) {
+            return true;
+        }
     }
 }
